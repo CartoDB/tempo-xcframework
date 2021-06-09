6eea011d6a7799e65be1ea6128e9ec00234011e7
Joaquin Perez <jperbar@me.com>
2021-06-09 11:52:02 +0200

    addXCFramework

---
+++ Tempo_ios_sdk.xcframework/ios-x86_64-simulator/Tempo_ios_sdk.framework/Headers/Tempo_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  Tempo_ios_sdk.h
+//  Tempo_ios_sdk
+//
+//  Created by Joaquin Perez on 02/06/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Tempo_ios_sdk.
+FOUNDATION_EXPORT double Tempo_ios_sdkVersionNumber;
+
+//! Project version string for Tempo_ios_sdk.
+FOUNDATION_EXPORT const unsigned char Tempo_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tempo_ios_sdk/PublicHeader.h>
+
+

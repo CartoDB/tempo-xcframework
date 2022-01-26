29b9ef9f7e105db588e2e5dea04984593de1f90a
Joaquin Perez Barroso <jperbar@me.com>
2022-01-26 11:39:09 +0100

    Version 2.0.1

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TempoSDK",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "TempoSDK",
+            targets: ["TempoSDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .binaryTarget(name: "TempoSDK", url: "https://storage.googleapis.com/tempo-xcframework/Tempo_ios_sdk.2.0.1.zip", checksum: "3bf35e7d5a7aa0bffc027ac7f8ad00d6a6783c3647ead97b7ab27ed5cfff23c4")
+    ]
+)
+
